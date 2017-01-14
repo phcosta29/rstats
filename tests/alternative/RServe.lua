cc39680f730ac9c66be8d9a9eed6d54cfc0ba018
phcosta29 <ph.oliveiracosta@gmail.com>
2017-01-13 23:30:42 -0200

    Update RServe.lua

--- tests/alternative/RServe.lua
+++ tests/alternative/RServe.lua
@@ -26,15 +26,6 @@ return {
 		end
 		unitTest:assertError(error_func, defaultValueMsg("port", 6311))
 
-		error_func = function()
-			RServe:evaluate("x = 2 + v")
-		end
-		unitTest:assertError(error_func, "[RServe] Error: object 'v' not found", 1)
-
-		error_func = function()
-			RServe:evaluate("x <- 1:10; y <- if (x < 5 ) 0 else 1")
-		end
-		unitTest:assertError(error_func, "[RServe] Warning: the condition has length > 1 and only the first element will be used", 1)
 		--error_func = function()
 			--RServe{port = 987654}
 		--end
@@ -46,6 +37,16 @@ return {
 		local error_func = function()
 			R:evaluate(2)
 		end
-		unitTest:assertError(error_func, incompatibleTypeMsg(1, "string", 2))
+		unitTest:assertError(error_func, incompatibleTypeMsg(1, "string", 2
+
+		error_func = function()
+			R:evaluate("x = 2 + v")
+		end
+		unitTest:assertError(error_func, "[RServe] Error: object 'v' not found", 1)
+
+		error_func = function()
+			R:evaluate("x <- 1:10; y <- if (x < 5 ) 0 else 1")
+		end
+		unitTest:assertError(error_func, "[RServe] Warning: the condition has length > 1 and only the first element will be used", 1)
 	end
 }
