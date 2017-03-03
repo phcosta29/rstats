704bc29ffbe3ad4441ded3258789d23001240b62
phcosta29 <ph.oliveiracosta@gmail.com>
2017-03-03 10:45:06 -0300

    Update RServe.lua

--- tests/basic/RServe.lua
+++ tests/basic/RServe.lua
@@ -208,11 +208,11 @@ return {
 	end,
 	lm = function(unitTest)
 		local R = RServe{}
-		local data = DataFrame{ctl = {4.17,5.58,5.18,6.11,4.50,4.61,5.17,4.53,5.33,5.14}, trt = {4.81,4.17,4.41,3.59,5.87,3.83,6.03,4.89,4.32,4.69}, weight = {4.17, 5.18, 4.50, 5.17, 5.33, 4.81, 4.41, 5.87, 4.89, 4.69}}
-		local x = R:lm{data = data, response = "ctl", terms = {"weight", "trt"}}
-		unitTest:assertEquals(x[1], 6.2664, 0.0001)
-		unitTest:assertEquals(x[2], 0.0647, 0.0001)
-		unitTest:assertEquals(x[3], -0.3329, 0.0001)
+		local data = DataFrame{ctl = {4.17,5.58,5.18,6.11,4.50,4.61,5.17,4.53,5.33,5.14}, trt = {4.81,4.17,4.41,3.59,5.87,3.83,6.03,4.89,4.32,4.69}, gfg = {4.16,5.57,5.16,6.08,4.49,4.60,5.14,4.51,5.32,5.13}, weight = {4.17, 5.18, 4.50, 5.17, 5.33, 4.81, 4.41, 5.87, 4.89, 4.69}}
+		local x = R:lm{data = data, response = "ctl", terms = {"weight", "trt", "gfg"}}
+		unitTest:assertEquals(x[1], -0.049973, 0.000001)
+		unitTest:assertEquals(x[2], 0.000062, 0.000001)
+		unitTest:assertEquals(x[3], 0.004117, 0.000001)
 	end,
 	__tostring = function(unitTest)
 		unitTest:assertEquals(tostring(RServe{}), [[host  string [localhost]
