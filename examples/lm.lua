501ef611a428743b1b97dbaff8ef19ddba236d69
phcosta29 <ph.oliveiracosta@gmail.com>
2017-08-01 16:22:51 -0300

    Update lm.lua

--- examples/lm.lua
+++ examples/lm.lua
@@ -1,13 +1,8 @@
 -- @example Implementation of the linear regression function.
 -- It returns the linear regression of a table of vectors computed in R.
 -- if an entry is of an incompatible type returns with error.
-<<<<<<< HEAD
 -- @arg expression a data frame or a CellularSpace.
 import("rstats")
-=======
--- @arg expression a DataFrame or a CellularSpace.
-
->>>>>>> 53f199203386cc11ad500b5e631121663120f374
 R = RServe{
 }
 
