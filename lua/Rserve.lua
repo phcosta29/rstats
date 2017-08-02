254133ef5e468008cf9e3e5d2852ccff54e87e4a
Pedro <ph.oliveiracosta@gmail.com>
2017-08-02 20:19:50 -0300

    Update of functions

--- lua/Rserve.lua
+++ lua/Rserve.lua
@@ -5,14 +5,14 @@ local QAP1_HEADER_FORMAT = "4*u4"
 local QAP1_PARAMETER_HEADER_FORMAT = "u1 u3"
 local QAP1_SEXP_HEADER_TYPE_FORMAT = "[1 | b2 u6]"
 local QAP1_SEXP_HEADER_LEN_FORMAT = "u3"
-local function vectorToString(expression)
-	local expressionR = table.concat{"c(", table.concat(expression, ","), ")"}
+local function vectorToString(data)
+	local expressionR = table.concat{"c(", table.concat(data, ","), ")"}
 	return expressionR
 end
 
-local function convertionTypes(expression)
+local function convertionTypes(data)
 	local df = {}
-	forEachElement(expression.data.cells[1], function(idx)
+	forEachElement(data.data.cells[1], function(idx)
 		if belong(idx, {"FID", "cObj_", "past"}) then
 			return
 		end
@@ -20,7 +20,7 @@ local function convertionTypes(expression)
 		df[idx] = {}
 	end)
 
-	forEachCell(expression.data, function(cell)
+	forEachCell(data.data, function(cell)
 		forEachElement(df, function(idx, values)
 			table.insert(values, cell[idx])
 		end)
@@ -34,106 +34,105 @@ Rserve_ = {
 	type_ = "Rserve",
 	--- Execute an R command. It returns an error message or a value.
 	-- if an entry is of an incompatible type returns with error.
-	-- @arg expression The expression must be passed to R.
+	-- @arg data The expression must be passed to R.
 	-- @usage import ("rstats")
 	-- R = Rserve{}
 	-- R:evaluate("x = 4")
-	evaluate = function(self, expression)
-		if type(expression) ~= "string" then
-			incompatibleTypeError(1, "string", expression)
+	evaluate = function(self, data)
+		if type(data) ~= "string" then
+			incompatibleTypeError(1, "string", data)
 		end
 
-		local result = luaRserveevaluate(self.host, self.port, "Sys.setenv(LANG='en'); tryCatch({"..expression.."}, warning = function(war){return(list(war,0))}, error = function(err){return(list(err,1))})")
+		local result = luaRserveevaluate(self.host, self.port, "Sys.setenv(LANG='en'); tryCatch({"..data.."}, warning = function(war){return(list(war,0))}, error = function(err){return(list(err,1))})")
 		if result[1] then
 			if result[1][1] and type(result[1][1]) ~= "number" and type(result[1][1]) ~= "string" and type(result[1][1]) ~= "boolean" then
 				if result[1][1][3] and type(result[1][1][3]) ~= "number" and type(result[1][1][3]) ~= "string" and type(result[1][1][3]) ~= "boolean" then
 					if result[1][1][3][1] == 1 then
-						return customError("[Rserve] Error: "..result[1][1][2][1][1])
-					else if result[1][1][3][1] == 0 then
-						return customWarning("[Rserve] Warning: "..result[1][1][2][1][1])
-					else
-						return result
-						end
+						customError("[Rserve] Error: "..result[1][1][2][1][1])
+					elseif result[1][1][3][1] == 0 then
+						customWarning("[Rserve] Warning: "..result[1][1][2][1][1])
 					end
-				else
-					return result
 				end
-			else
-				return result
 			end
-		else
-			return result
 		end
+		
+		while type(result) == "table" and #result == 1 do
+			result = result[1]
+		end
+
+		return result
 	end,
 
 	--- It returns the arithmetic mean of a vector of values computed in R.
 	-- if an entry is of an incompatible type returns with error.
-	-- @arg expression a table of numbers.
+	-- @arg data a table of numbers.
 	-- @usage import ("rstats")
 	-- R = Rserve{}
 	-- R:mean{1,2,3,4,5,6,7,8,9,10} -- 5.5
-	mean = function(self, expression)
-		if type(expression) ~= "table" then
-			incompatibleTypeError(1, "table", expression)
+	mean = function(self, data)
+		if type(data) ~= "table" then
+			incompatibleTypeError(1, "table", data)
 		end
 
-		local expressionR = table.concat{"mean(", vectorToString(expression), ")"}
+		local expressionR = table.concat{"mean(", vectorToString(data), ")"}
 		local result = self:evaluate(expressionR)
-		return result[1][1][1]
+		return result
 	end,
 
 	--- It returns the standard deviation of a vector of values computed in R.
 	-- if an entry is of an incompatible type returns with error.
-	-- @arg expression a table of numbers.
+	-- @arg data a table of numbers.
 	-- @usage import ("rstats")
 	-- R = Rserve{}
 	-- R:sd{1,2,3,4,5,6,7,8,9,10} -- 3.02765
-	sd = function(self, expression)
-		if type(expression) ~= "table" then
-			incompatibleTypeError(1, "table", expression)
+	sd = function(self, data)
+		if type(data) ~= "table" then
+			incompatibleTypeError(1, "table", data)
 		end
 
-		local expressionR = table.concat{"sd(", vectorToString(expression), ")"}
+		local expressionR = table.concat{"sd(", vectorToString(data), ")"}
 		local result = self:evaluate(expressionR)
-		return result[1][1][1]
+		return result
 	end,
 
 	--- It returns the linear regression of a table of vectors computed in R.
 	-- if an entry is of an incompatible type returns with error.
-	-- @arg expression a DataFrame or a CellularSpace.
+	-- @arg data.data a DataFrame or a CellularSpace.
+	-- @arg data.response Column of DataFrame or CellularSpace.
+	-- @arg data.terms Columns of DataFrame or CellularSpace.
 	-- @usage import ("rstats")
 	-- R = Rserve{}
 	-- data = CellularSpace{file = filePath("amazonia.shp", "base"),}
 	-- R:lm{data = data, response = "prodes_10", terms = {"distroads", "protected", "distports"}} -- 22.4337, -8.7823e-05, -11.9502
-	lm = function(self, expression)
-		if type(expression) ~= "table" then
-			incompatibleTypeError(1, "table", expression)
-		elseif type(expression.data) == "CellularSpace" then
-			expression.data = convertionTypes(expression)
+	lm = function(self, data)
+		if type(data) ~= "table" then
+			incompatibleTypeError(1, "table", data)
+		elseif type(data.data) == "CellularSpace" then
+			data.data = convertionTypes(data)
 		end
 
-		local term = #expression.terms
+		local term = #data.terms
 		local stri, df, sumTerms
-		local str = vectorToString(expression.data[expression.response])
+		local str = vectorToString(data.data[data.response])
 		local i = term
 		while i > 0 do
-			local t = vectorToString(expression.data[expression.terms[i]])
+			local t = vectorToString(data.data[data.terms[i]])
 			if i == term then
-				stri = table.concat{expression.terms[i], " <- ", t, ";"}
+				stri = table.concat{data.terms[i], " <- ", t, ";"}
 			else
-				stri = table.concat{stri, expression.terms[i], " <- ", t, ";"}
+				stri = table.concat{stri, data.terms[i], " <- ", t, ";"}
 			end
 
 			i = i - 1
 		end
 
-		df = table.concat{expression.response, " = ", expression.response, ", "}
+		df = table.concat{data.response, " = ", data.response, ", "}
 		i = term
 		while i > 0 do
 			if i > 1 then
-				df = table.concat{df, expression.terms[i], " = ", expression.terms[i], ", "}
+				df = table.concat{df, data.terms[i], " = ", data.terms[i], ", "}
 			else
-				df = table.concat{df, expression.terms[i], " = ", expression.terms[i], "); result = lm(formula = "}
+				df = table.concat{df, data.terms[i], " = ", data.terms[i], "); result = lm(formula = "}
 			end
 
 			i = i - 1
@@ -142,51 +141,54 @@ Rserve_ = {
 		i = 1
 		while i <= term do
 			if i == 1 then
-				sumTerms = table.concat{expression.terms[i], " + "}
+				sumTerms = table.concat{data.terms[i], " + "}
 			else
 				if i == term then
-					sumTerms = table.concat{sumTerms, expression.terms[i]}
+					sumTerms = table.concat{sumTerms, data.terms[i]}
 				else
-					sumTerms = table.concat{sumTerms, expression.terms[i], " + "}
+					sumTerms = table.concat{sumTerms, data.terms[i], " + "}
 				end
 			end
 
 			i = i + 1
 		end
 
-		local result = self:evaluate(table.concat{expression.response, " <- ", str, "; ", stri, "; df = data.frame(", df, expression.response, " ~ ", sumTerms, ", data = df)"})
-		local resultTable = {result[1][2][2][1], result[1][2][2][2], result[1][2][2][3]}
+		local result = self:evaluate(table.concat{data.response, " <- ", str, "; ", stri, "; df = data.frame(", df, data.response, " ~ ", sumTerms, ", data = df)"})
+		local resultTable = {result[2][2][1], result[2][2][2], result[2][2][3]}
 		return resultTable
 	end,
 
 	--- It returns the principal component analysis of a table of vectors computed in R.
 	-- if an entry is of an incompatible type returns with error.
-	-- @arg expression a DataFrame or a CellularSpace.
+	-- @arg data.data a DataFrame or a CellularSpace.
+	-- @arg data.terms Columns of DataFrame or CellularSpace.
 	-- @usage import ("rstats")
 	-- R = Rserve{}
-	-- data = DataFrame{ctl = {4.17, 5.58, 5.18, 6.11, 4.50, 4.61, 5.17, 4.53, 5.33, 5.14}, trt = {4.81, 4.17, 4.41, 3.59, 5.87, 3.83, 6.03, 4.89, 4.32, 4.69}, weight = {4.17, 5.18, 4.50, 5.17, 5.33, 4.81, 4.41, 5.87, 4.89, 4.69}}
+	-- data = DataFrame{ctl = {4.17, 5.58, 5.18, 6.11, 4.50, 4.61, 5.17, 4.53, 5.33, 5.14}, 
+	-- trt = {4.81, 4.17, 4.41, 3.59, 5.87, 3.83, 6.03, 4.89, 4.32, 4.69}, 
+	-- weight = {4.17, 5.18, 4.50, 5.17, 5.33, 4.81, 4.41, 5.87, 4.89, 4.69}}
 	-- R:pca{data = data, terms = {"ctl", "trt", "weight"}} -- 1.2342, 0.9735, 0.7274
-	pca = function(self, expression)
-		if type(expression) ~= "table" then
-			incompatibleTypeError(1, "table", expression)
-		elseif type(expression.data) == "CellularSpace" then
-			expression.data = convertionTypes(expression)
+	pca = function(self, data)
+		if type(data) ~= "table" then
+			incompatibleTypeError(1, "table", data)
+		elseif type(data.data) == "CellularSpace" then
+			data.data = convertionTypes(data)
 		end
 
-		local term = #expression.terms
+		local term = #data.terms
 		local str, df, i, j, resultTable
 		local rotation = {}
 		i = 1
 		while i <= term do
 			if i == 1 then
-				str = table.concat{expression.terms[i], " <- ", vectorToString(expression.data[expression.terms[i]]), "; "}
-				df = table.concat{expression.terms[i], " = ", expression.terms[i], ", "}
+				str = table.concat{data.terms[i], " <- ", vectorToString(data.data[data.terms[i]]), "; "}
+				df = table.concat{data.terms[i], " = ", data.terms[i], ", "}
 			else
-				str = table.concat{str, expression.terms[i], " <- ", vectorToString(expression.data[expression.terms[i]]), "; "}
+				str = table.concat{str, data.terms[i], " <- ", vectorToString(data.data[data.terms[i]]), "; "}
 				if i ~= term then
-					df = table.concat{df, expression.terms[i], " = ", expression.terms[i], ", "}
+					df = table.concat{df, data.terms[i], " = ", data.terms[i], ", "}
 				else
-					df = table.concat{df, expression.terms[i], " = ", expression.terms[i], "); "}
+					df = table.concat{df, data.terms[i], " = ", data.terms[i], "); "}
 				end
 			end
 
@@ -197,68 +199,101 @@ Rserve_ = {
 		i = 1
 		while i <= term do
 			j = 0
-			rotation[i] = {expression.terms[i],{}}
+			rotation[i] = {data.terms[i],{}}
 			while j <= term do
-				table.insert(rotation[i][2], result[1][2][3][(term * j) + i])
+				table.insert(rotation[i][2], result[2][3][(term * j) + i])
 				j = j + 1
 			end
 
 			i = i + 1
 		end
 
-		resultTable = {StandardDeviations = result[1][2][1], Rotation = rotation}
+		resultTable = {StandardDeviations = result[2][1], Rotation = rotation}
 		return resultTable
 	end,
 
 	--- It returns the analysis of variance of a table of vectors computed in R.
 	-- if an entry is of an incompatible type returns with error.
-	-- @arg expression a DataFrame or a CellularSpace.
+	-- @arg data.data a DataFrame or a CellularSpace.
+	-- @arg data.terms Columns of DataFrame or CellularSpace.
+	-- @arg data.strategy Type of ANOVA model to be used.
+	-- @arg data.factors Columns of DataFrame or CellularSpace that will be the factors.
+	-- @tabular strategy
+	-- Strategy & Description \
+	-- "owa" & One Way Anova (Completely Randomized Design) \
+	-- "rbd" & Randomized Block Design (B is the blocking factor) \
+	-- "twfd" & Two Way Factorial Design \
+	-- "aoc" & Analysis of Covariance \
+	-- "owf" & One Within Factor \
+	-- ""twftbf" & Two Within Factors, Two Between Factors \
 	-- @usage import ("rstats")
 	-- R = Rserve{}
-	-- data = DataFrame{ctl = {4.17, 5.58, 5.18, 6.11, 4.50, 4.61, 5.17, 4.53, 5.33, 5.14}, trt = {4.81, 4.17, 4.41, 3.59, 5.87, 3.83, 6.03, 4.89, 4.32, 4.69}, weight = {4.17, 5.18, 4.50, 5.17, 5.33, 4.81, 4.41, 5.87, 4.89, 4.69}}
-	-- R:anova{data = data, terms = {"ctl", "trt", "weight"}, typeAnova = "owa", factors = {"ctl", "trt"}} -- 1.0, 8.0, 0.6409, 2.4190, 0.6409, 0.3023, 2.1196, 0.1835
-	anova = function(self, expression)
-		if type(expression) ~= "table" then
-			incompatibleTypeError(1, "table", expression)
-		elseif type(expression.data) == "CellularSpace" then
-			expression.data = convertionTypes(expression)
+	-- data = DataFrame{ctl = {4.17, 5.58, 5.18, 6.11, 4.50, 4.61, 5.17, 4.53, 5.33, 5.14}, 
+	-- trt = {4.81, 4.17, 4.41, 3.59, 5.87, 3.83, 6.03, 4.89, 4.32, 4.69}, 
+	-- weight = {4.17, 5.18, 4.50, 5.17, 5.33, 4.81, 4.41, 5.87, 4.89, 4.69}}
+	-- R:anova{data = data, terms = {"ctl", "trt", "weight"}, strategy = "owa", factors = {"ctl", "trt"}} -- 1.0, 8.0, 0.6409, 2.4190, 0.6409, 0.3023, 2.1196, 0.1835
+	anova = function(self, data)
+		if type(data) ~= "table" then
+			incompatibleTypeError(1, "table", data)
+		elseif type(data.data) == "CellularSpace" then
+			data.data = convertionTypes(data)
 		end
 
-		local term = #expression.terms
+		local term = #data.terms
 		local str, df, i, fit
 		i = 1
 		while i <= term do
 			if i == 1 then
-				str = table.concat{expression.terms[i], " <- ", vectorToString(expression.data[expression.terms[i]]), "; "}
-				df = table.concat{expression.terms[i], " = ", expression.terms[i], ", "}
+				str = table.concat{data.terms[i], " <- ", vectorToString(data.data[data.terms[i]]), "; "}
+				df = table.concat{data.terms[i], " = ", data.terms[i], ", "}
 			else
-				str = table.concat{str, expression.terms[i], " <- ", vectorToString(expression.data[expression.terms[i]]), "; "}
+				str = table.concat{str, data.terms[i], " <- ", vectorToString(data.data[data.terms[i]]), "; "}
 				if i ~= term then
-					df = table.concat{df, expression.terms[i], " = ", expression.terms[i], ", "}
+					df = table.concat{df, data.terms[i], " = ", data.terms[i], ", "}
 				else
-					df = table.concat{df, expression.terms[i], " = ", expression.terms[i], "); "}
+					df = table.concat{df, data.terms[i], " = ", data.terms[i], "); "}
 				end
 			end
 
 			i = i + 1
 		end
 
-		switch(expression, "typeAnova"):caseof{
-			owa = function() fit = table.concat{"fit <- aov(", expression.factors[1], " ~ ", expression.factors[2], ",data = df); x = summary(fit); x;"} end,
-			rbd = function() fit = table.concat{"fit <- aov(", expression.factors[1], " ~ ", expression.factors[2], " + ", expression.factors[3], ",data = df); x = summary(fit); x;"} end,
-			twfd = function() fit = table.concat{"fit <- aov(", expression.factors[1], " ~ ", expression.factors[2], " * ", expression.factors[3], ",data = df); x = summary(fit); x;"} end,
-			aoc = function() fit = table.concat{"fit <- aov(", expression.factors[1], " ~ ", expression.factors[2], " + ", expression.factors[3], ",data = df); x = summary(fit); x;"} end,
-			owf = function() fit = table.concat{"fit <- aov(", expression.factors[1], " ~ ", expression.factors[2], " + Error(Subject/", expression.factors[2], "),data = df); x = summary(fit); x;"} end,
-			twftbf = function() fit = table.concat{"fit <- aov(", expression.factors[1], " ~ (", expression.factors[2], " * ", expression.factors[3], " * ", expression.factors[4], " * ", expression.factors[5], ") + Error(Subject/(", expression.factors[2], " * ", expression.factors[3], ")) + (", expression.factors[4], " * ", expression.factors[5] "),data = df); x = summary(fit); x;"} end
+		switch(data, "strategy"):caseof{
+			owa = function() fit = table.concat{"fit <- aov(", data.factors[1], " ~ ", data.factors[2], ",data = df); x = summary(fit); x;"} end,
+			rbd = function() fit = table.concat{"fit <- aov(", data.factors[1], " ~ ", data.factors[2], " + ", data.factors[3], ",data = df); x = summary(fit); x;"} end,
+			twfd = function() fit = table.concat{"fit <- aov(", data.factors[1], " ~ ", data.factors[2], " * ", data.factors[3], ",data = df); x = summary(fit); x;"} end,
+			aoc = function() fit = table.concat{"fit <- aov(", data.factors[1], " ~ ", data.factors[2], " + ", data.factors[3], ",data = df); x = summary(fit); x;"} end,
+			owf = function() fit = table.concat{"fit <- aov(", data.factors[1], " ~ ", data.factors[2], " + Error(Subject/", data.factors[2], "),data = df); x = summary(fit); x;"} end,
+			twftbf = function() fit = table.concat{"fit <- aov(", data.factors[1], " ~ (", data.factors[2], " * ", data.factors[3], " * ", data.factors[4], " * ", data.factors[5], ") + Error(Subject/(", data.factors[2], " * ", data.factors[3], ")) + (", data.factors[4], " * ", data.factors[5] "),data = df); x = summary(fit); x;"} end
 		}
 		local result = self:evaluate(table.concat{str, "df = data.frame(", df, fit})
 		return result
 	end
 
+	--declare = function(self, data)
+	--	if type(data) ~= "table" then
+	--		incompatibleTypeError(1, "table", data)
+	--	elseif type(data.data) == "CellularSpace" then
+	--		data.data = convertionTypes(data)
+	--	end
+	--	return data.data
+		--local result = self:evaluate(table.concat(data.index, " = ", data.data))
+		--return result
+	--end
 }
 metaTableRserve_ = {
-	__index = Rserve_,
-	__tostring = _Gtme.tostring
+	__index = function(self, index)
+		if Rserve_[index] then 
+			return Rserve_[index] 
+		end
+			return function(self, value)
+				return self:evaluate(index.."("..value..")")
+			end
+	end,
+	__tostring = _Gtme.tostring,
+	--__newindex = function(self, index, value)
+		--return self:declare({index = index, data = value})
+	--end
 }
 --- Establishes host and port for communication with R.
 -- if an entry is of an incompatible type returns with error.
